7d5bb4d3839066810980f0ecfca4be84d011c40f
makisovall-png <makisovall@gmail.com>
2026-02-04 13:23:36 +0300

    added lesson 1b

---
+++ lesson1b.py
@@ -0,0 +1,16 @@
+#Python Data Types
+#String : It is a series of characters that are inside a qoute
+name = "Dalphine Chepchirchir"
+print(name)
+print(type(name))
+
+#Numerics 
+#INT: it indicates a whole number in python. For an integer we don't use quotes
+age = 18
+print("The age of the person is :", age)
+print(type(age))
+
+#Float - THis is a number with a decimal
+coordinates = 16.098
+print("The coordinates are:", coordinates)
+print(type(coordinates))
